67616176cc331cab5944695c0023e60c389aa2da
alixchan <101437194+alixchan@users.noreply.github.com>
2023-03-29 11:01:15 +0300

    Add swagger descriptions in main

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"GoJira/pkg/connector"
+
+	_ "github.com/swaggo/swag"
 )
+
+//	@title			JIRA_analizer
+//	@version		1.0
+//	@description	Разработка промышленного клиент-серверного приложения с применением принципов микросервисной архитектуры, языков программирования Golang, фрейморка Angular и TypeScript.
+//  @BasePath /
+
+//	@license.name	MIT License
 
 func main() {
 	connector.DownloadProjects()
